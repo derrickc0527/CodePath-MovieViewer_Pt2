e8de33b810e1d0aef9c30ba8b618e5a9b29a6800
Derrick <derrickc0527@gmail.com>
2017-02-10 20:59:14 -0500

    upload code from previous and continue working on it

---
+++ MovieViewer/MovieViewer/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Derrick Chong on 1/31/17.
+//  Copyright © 2017 DerrickCorp. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var imagewindow: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    } 
+
+}
